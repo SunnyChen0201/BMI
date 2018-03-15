2e049f390abcf581d9528e7b2e502d084b10d4bd
m305-29\w401 <sunny1349@gmail.com>
2018-03-15 18:23:04 +0800

    第一次

---
+++ test1/test1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("┌────────────┐");
+            Console.WriteLine("│                        │");
+            Console.WriteLine("│                        │");
+            Console.WriteLine("│          B M I         │");
+            Console.WriteLine("│                        │");
+            Console.WriteLine("│                        │");
+            Console.WriteLine("└────────────┘");
+
+
+
+
+            Console.WriteLine("輸入體重(公斤) : ");
+            int KG = int.Parse(Console.ReadLine());
+            Console.WriteLine("輸入身高(公尺) : ");
+            double M = double.Parse(Console.ReadLine());
+            double BMI = KG / (M * M);
+            if (BMI >24)
+                Console.WriteLine("您過重ㄌ"+"您的BMI:"+BMI);
+            if (BMI<18.5)
+                Console.WriteLine("您過輕ㄌ" + "您的BMI:" + BMI);
+
+
+            else
+                Console.WriteLine("正常" + "您的BMI:" + BMI);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
